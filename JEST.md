21a546279236a86cca76d16ef1e5683a332bfade
michael19 <rekmixa@gmail.com>
2021-03-24 00:28:00 +0500

    change hello-world test

--- JEST.md
+++ JEST.md
@@ -1,3 +1,5 @@
+# Vue Test Utils & Jest
+
 ```bash
 yarn add @vue/test-utils vue-jest jest --dev
 yarn add babel-jest @babel/core @babel/preset-env babel-core@^7.0.0-bridge.0 babel-polyfill --dev
--- tests/unit/HelloWorld.test.js
+++ tests/unit/HelloWorld.test.js
@@ -2,14 +2,16 @@
 import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-test('displays message', () => {
-  // mount() returns a wrapped Vue component we can interact with
-  const wrapper = mount(HelloWorld, {
-    propsData: {
-      msg: 'Hello world'
-    }
+describe('HelloWorld.vue', () => {
+  it('render message', () => {
+    // mount() returns a wrapped Vue component we can interact with
+    const wrapper = mount(HelloWorld, {
+      propsData: {
+        msg: 'Hello world'
+      }
+    })
+  
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('Hello world')
   })
-
-  // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('Hello world')
 })
