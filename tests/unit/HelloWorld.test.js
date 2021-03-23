e13e301d74ca60d841e5922601c329011d72588f
michael19 <rekmixa@gmail.com>
2021-03-24 00:28:23 +0500

    change hello-world test

--- tests/unit/HelloWorld.test.js
+++ tests/unit/HelloWorld.test.js
@@ -1,17 +1,14 @@
-// Import the `mount()` method from Vue Test Utils
 import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('render message', () => {
-    // mount() returns a wrapped Vue component we can interact with
     const wrapper = mount(HelloWorld, {
       propsData: {
         msg: 'Hello world'
       }
     })
   
-    // Assert the rendered text of the component
     expect(wrapper.text()).toContain('Hello world')
   })
 })
